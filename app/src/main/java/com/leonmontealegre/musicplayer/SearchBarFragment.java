57f69eaa40d534542568ee248d69047297844357
KingDolphin <kingdolphinguy@gmail.com>
2016-02-05 08:08:09 -0800

    Add search bar

---
+++ app/src/main/java/com/leonmontealegre/musicplayer/SearchBarFragment.java
@@ -0,0 +1,23 @@
+package com.leonmontealegre.musicplayer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class SearchBarFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_search_bar, container, false);
+
+
+
+        return rootView;
+    }
+}
